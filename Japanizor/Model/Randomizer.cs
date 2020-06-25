e2c33c191becbfb345870eda3e2188c0b773c78f
rmtz <rmtz@outlook.com>
2020-06-25 19:03:43 +0200

    Added looping logic at test end and excluded routes.json from service worker assets.

--- Japanizor/Model/Randomizer.cs
+++ Japanizor/Model/Randomizer.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Components.Web;
+
 namespace Japanizor.Model
 {
     public class Randomizer
     {
         public static void Randomize<T>(T[] items)
         {
-            Random rand = new Random();
+            T temp;
+            var rand = new Random();
 
-            // For each spot in the array, pick
-            // a random item to swap into that spot.
             for (int i = 0; i < items.Length - 1; i++)
             {
                 int j = rand.Next(i, items.Length);
-                T temp = items[i];
+                temp = items[i];
                 items[i] = items[j];
                 items[j] = temp;
             }
